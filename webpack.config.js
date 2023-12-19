b4001285e803bf1da12af9a7bc2615d57dea69c5
GazzaBombata <giorgio.giotto.gg@gmail.com>
2023-12-19 19:21:41 +0100

    second commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Output",
+    }),
+  ],
+};
+
+entry: {
+    main: path.resolve(__dirname, './src/app.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'deploy')
+  },
